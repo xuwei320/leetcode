98dc3ea6046868936807ee88fa9bad21bef4e97d
xuwei <xuwei320@qq.com>
2017-03-07 23:34:50 -0800

    第一次提交，return的条件错了

---
+++ 117.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+/**
+ * Definition for binary tree with next pointer.
+ * struct TreeLinkNode {
+ *  int val;
+ *  TreeLinkNode *left, *right, *next;
+ *  TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+ * };
+ */
+struct TreeLinkNode {
+	int val;
+    TreeLinkNode *left, *right, *next;
+    TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+};
+class Solution {
+public:
+    void connect(TreeLinkNode *root) {
+        if (!root) return;
+        TreeLinkNode* parent = root;
+        while (parent || parent->next) {
+            TreeLinkNode* pSon = NULL, *pNode = parent;
+            while (pNode) {
+                if (pNode->left && pNode->right) pNode->left->next = pNode->right;
+                if (pNode->next) pNode = pNode->next;
+            }
+            pNode = parent;
+            while (!pSon) {
+                pSon = pNode->left == NULL ? pNode->right : pNode->left;
+                if (!pSon && !pNode->next) return;
+                else if (!pSon) pNode = pNode->next;
+            }//pNode 指向第一个有子节点的节点
+            if (!pNode) return;
+            while (pNode->next) {
+                TreeLinkNode* first = pNode->right ? pNode->right : pNode->left;
+                TreeLinkNode* nextparent = pNode->next;
+                while (!nextparent->left && !nextparent->right) {
+                    nextparent = nextparent->next;
+                    if (!nextparent) return;
+                }
+                TreeLinkNode* next = nextparent->left ? nextparent->left : nextparent->right;
+                first->next = next;
+                pNode = nextparent;
+            }
+            parent = pSon;
+        }
+    }
+};
+
